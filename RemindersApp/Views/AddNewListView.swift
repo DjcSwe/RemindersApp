8afc7191603d7db43772ae2460b7781011e72866
Daniel Condly <settle.never@aol.com>
2024-09-05 11:13:02 -0400

    Implementing AddNewList View User Interface

---
+++ RemindersApp/Views/AddNewListView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewListView.swift
+//  RemindersApp
+//
+//  Created by DJC on 9/5/24.
+//
+
+import SwiftUI
+
+struct AddNewListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewListView()
+}
---
+++ RemindersApp/Views/ColorPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPickerView.swift
+//  RemindersApp
+//
+//  Created by DJC on 9/5/24.
+//
+
+import SwiftUI
+
+struct ColorPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorPickerView()
+}
